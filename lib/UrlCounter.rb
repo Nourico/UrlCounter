1fcc7bc2ad658a20d0b8bdf48d411cf9af7d53fc
Hassan Nouri <h.nouri85@gmail.com>
2017-12-05 17:47:24 -0500

    puts instead of put

--- lib/UrlCounter.rb
+++ lib/UrlCounter.rb
@@ -2,8 +2,11 @@ require "UrlCounter/version"
 
 module UrlCounter
   def self.count(url, date)
-      UrlCounter::Url.where(:address => url)
+    UrlCounter::Url.where(:address => url, :visit_date => date).size
   end
 
 
+  def self.present_urls()
+    render html: '<div>html goes here</div>'.html_safe
+  end
 end
