857f8c326c4ece62a43507bf65958ed4b96a3bc4
Hassan Nouri <h.nouri85@gmail.com>
2017-12-05 23:24:11 -0500

    puts instead of put

--- lib/UrlCounter.rb
+++ lib/UrlCounter.rb
@@ -1,12 +1,37 @@
 require "UrlCounter/version"
+require 'csv'
 
 module UrlCounter
   def self.count(url, date)
-    Url.where(:address => url, :visit_date => date).size
+    url + ', ' + (Url.where('address LIKE ?', url).where(:visit_date => date).size).to_s + ', ' + date
   end
 
 
-  def self.present_urls()
-    render html: '<div>html goes here</div>'.html_safe
+  def self.listUrl(*urls, startDate, endDate)
+    theList = Array.new
+    urls.each do |url|
+      (startDate..endDate).each do |date|
+        theList << self.count(url, date)
+      end
+    end
+    theList
   end
+
+  def self.presentHTML(rows)
+    htmlCode = ''
+    rows.each do |row|
+      htmlCode += '<div>' + row + '<div><br>'
+    end
+    htmlCode
+
+  end
+
+  def self.exportCSV(rows)
+    CSV.generate do |csv|
+      rows.each do |row|
+        csv << row.split(',')
+      end
+    end
+  end
+
 end
--- lib/UrlCounter/version.rb
+++ lib/UrlCounter/version.rb
@@ -1,3 +1,3 @@
 module UrlCounter
-  VERSION = "0.1.18"
+  VERSION = "0.1.19"
 end
